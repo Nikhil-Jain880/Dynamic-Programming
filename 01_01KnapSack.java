4ddf7c7ce43e0cb3017ccf31f30bd93866654b63
Nikhil Jain <jainnikhil880@gmail.com>
2022-04-01 16:57:43 +0530

    Memoization added

--- 01_01KnapSack.java
+++ 01_01KnapSack.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 // https://practice.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1#
 
 class KnapSack {
@@ -24,14 +26,48 @@ class KnapSack {
                                                      // then skip/don't take and move on
     }
 
+    // Memoization
+
+    static int[][] dp; // Global Variable to store the maximum value for the situation(weight and
+                       // index)
+    // Bottom up approach - approaching the problem from the back we are storing the
+    // value in the dp from the back
+    // Function to return max value that can be put in knapsack of capacity W.
+
+    static int knapSackMemoization(int W, int wt[], int val[], int n) {
+        dp = new int[W + 1][n + 1];
+        int ans = helper(W, wt, val, n);
+        return ans;
+    }
+
+    static int helper(int weight, int[] wt, int[] val, int n) {
+        if (n == 0 || weight == 0) {
+            return 0;
+        }
+        if (dp[weight][n - 1] != 0)
+            return dp[weight][n - 1];
+        if (wt[n - 1] <= weight) {
+            return dp[weight][n - 1] = Math.max(val[n - 1] + helper(weight - wt[n - 1], wt, val, n - 1),
+                    helper(weight, wt, val, n - 1));
+        } else
+            return helper(weight, wt, val, n - 1);
+    }
+
+    // Main Method
     public static void main(String[] args) {
 
         int N = 3;
-        int W = 4;
+        int W = 5;
         int values[] = { 1, 2, 3 };
         int weight[] = { 4, 5, 1 };
 
-        int ans = knapSack(W, weight, values, N);
-        System.out.println(ans);
+        // int ans = knapSack(W, weight, values, N); // Recursion
+        int ansMemoization = knapSackMemoization(W, weight, values, N); // Memoization
+
+        for (int[] i : dp) {
+            System.out.println(Arrays.toString(i));
+        }
+        // System.out.println(ans);
+        System.out.println(ansMemoization);
     }
 }
