e5971af9154634d6aa5f72d0c3501f46cf179dd0
Nikhil Jain <jainnikhil880@gmail.com>
2022-04-27 01:52:15 +0530

    V** IMp concept

--- 37_Longest_Common_Subsequence.java
+++ 37_Longest_Common_Subsequence.java
@@ -1,3 +1,25 @@
+// https://leetcode.com/problems/longest-common-subsequence/
+
+
+/**
+ * For every Instant we have two Futures :- both the characters are same, They are not same 
+ * When both the characters are same 
+ * - Increment the ans by 1 and move(decrement) both the pointer
+ * - func(s1, s2, m-1, n-1) + 1
+ * When both the characters are not same we again are faced with a dilema which one to decrement m or n
+ * - call the function two times in which keep m as same and decrement n & see what happens
+ * - call the function two times in which keep n as same and decrement m & see what happens
+ * - Take the max of two
+ * - max(func(s1, s2, m, n-1), func(s1, s2, m-1, n)) 
+ * 
+ * to prove the above claim suppose we have inputs as s1 = abcg, s2 = abc; m for s1 and n for s2
+ * both g and c are not same here the best way would be to decrement g(m pointer) so that the value become c
+ * but the situation can be reversed also we cant always deceremnt m, there may be a case further in the array
+ * in which decrementing n whould have been more fruitfull, so we decrement both seperately and check for all 
+ * possible cases 
+ */
+
+
 import java.util.Arrays;
 
 class Longest_Common_Subsequence {
@@ -9,14 +31,15 @@ class Longest_Common_Subsequence {
         return ans;
     }
     
+    // m & n denotes the len of string when they are at 0 that means there is no char left to explore
     private static int helperRecursion(String str1, String str2, int m, int n)
     {
-        if(m == 0 || n ==0) return 0;
+        if(m == 0 || n ==0) return 0; // Base case
         
-        if(str1.charAt(m-1) == str2.charAt(n-1))
-        {
+        if(str1.charAt(m-1) == str2.charAt(n-1)) // Correction for 0 based indexing to get nth char you need to ask for n-1 
+        { // if both the char are same add one to the ans and decrement both char
             return 1 + helperRecursion(str1, str2, m-1, n-1);
-        }
+        } // Else keep n in its place and decrement m and vice versa and pick the one with most value
         return Math.max(helperRecursion(str1, str2, m-1,n), helperRecursion(str1, str2, m, n-1));
     }
 
@@ -60,8 +83,8 @@ class Longest_Common_Subsequence {
         int n = str2.length();
         int[][] dp = new int[m+1][n+1];
         
-        int longestLength = 0;
-        
+        // for all i ==0  and all j ==0 the value will be zero as they indicate that there are no elements to compare 
+        // Check the base case of recursion
         for(int i = 1;i<=m;i++)
         {
             for(int j =1;j<=n;j++)
@@ -72,7 +95,7 @@ class Longest_Common_Subsequence {
                 }
                 else 
                 {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]); // (m-1, n) + (m, n-1)
                 }
             }
         }
